33e19f7cd3b6c96e0b9a8e0d4f73f9360d2c4ed3
David <dromeo89@hotmail.com>
2026-03-10 21:40:20 +1100

    ui fixes 3

--- app.js
+++ app.js
@@ -82,6 +82,36 @@ img.src=heroImage(hero.name)
 
 div.appendChild(img)
 
+/* TOOLTIP */
+
+div.addEventListener("mouseenter",(e)=>{
+
+let tooltip=document.createElement("div")
+tooltip.className="heroTooltip"
+
+tooltip.innerHTML=
+`${hero.displayName}<br>Role: ${hero.roles[0]} / ${hero.roles[1]}`
+
+document.body.appendChild(tooltip)
+
+let rect=e.target.getBoundingClientRect()
+
+tooltip.style.left=(rect.left)+"px"
+tooltip.style.top=(rect.top-35)+"px"
+
+div.tooltip=tooltip
+
+})
+
+div.addEventListener("mouseleave",()=>{
+
+if(div.tooltip){
+div.tooltip.remove()
+div.tooltip=null
+}
+
+})
+
 div.onclick=()=>addHero(hero.hero_id,true)
 
 div.oncontextmenu=(e)=>{
