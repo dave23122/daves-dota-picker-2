c7e7ff58eb430addeb4f5a4bd64f9425b288f5f5
David <dromeo89@hotmail.com>
2026-03-13 10:38:11 +1100

    positioning

--- app.js
+++ app.js
@@ -156,8 +156,8 @@ document.body.appendChild(tooltip)
 
 let rect=e.target.getBoundingClientRect()
 
-tooltip.style.left=(rect.left)+"px"
-tooltip.style.top=(rect.top-35)+"px"
+tooltip.style.left = e.clientX + 10 + "px";
+tooltip.style.top = e.clientY - 28 + "px";
 
 element.tooltip=tooltip
 
